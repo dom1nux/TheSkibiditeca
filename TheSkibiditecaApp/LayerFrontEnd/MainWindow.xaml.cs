b675aa0149149cc2624a067ca0f138ec3e87c3a8
Luis <luisirigoin123@gmail.com>
2025-01-25 16:09:51 -0500

    Responsive Changes

--- TheSkibiditecaApp/LayerFrontEnd/MainWindow.xaml.cs
+++ TheSkibiditecaApp/LayerFrontEnd/MainWindow.xaml.cs
@@ -14,17 +14,13 @@ namespace TheSkibiditecaApp {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            
+            this.Title = "Iniciar Sesión";
+            Uri path = new(@"./Pages/Login.xaml", UriKind.Relative);
+            fra_main.Source = path;
         }
 
         private void Window_Closed(object sender, EventArgs e) {
             Application.Current.Shutdown();
-        }
-
-        private void Window_Activated(object sender, EventArgs e) {
-            this.Title = "Iniciar Sesión";
-            Uri path = new(@"./Pages/Login.xaml", UriKind.Relative);
-            fra_main.Source = path;
         }
     }
 }
