7f8a420d1c149da32c9e68ea49e8ed3a1cae0cd4
Luis <luisirigoin123@gmail.com>
2025-01-26 11:30:18 -0500

    Added UserControl System

---
+++ TheSkibiditecaApp/LayerFrontEnd/Controls/Backgrounds/BCK_Manager.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LayerFrontEnd.Controls.Backgrounds
+{
+    /// <summary>
+    /// Lógica de interacción para BCK_Manager.xaml
+    /// </summary>
+    public partial class BCK_Manager : UserControl
+    {
+        public BCK_Manager()
+        {
+            InitializeComponent();
+        }
+    }
+}
