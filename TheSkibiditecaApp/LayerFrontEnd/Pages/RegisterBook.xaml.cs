87522e239db0489d067f3ae13313ffe53a2b331b
Luis <luisirigoin123@gmail.com>
2025-01-30 15:27:12 -0500

    Alternate UserControls of RegisterBook

--- TheSkibiditecaApp/LayerFrontEnd/Pages/RegisterBook.xaml.cs
+++ TheSkibiditecaApp/LayerFrontEnd/Pages/RegisterBook.xaml.cs
@@ -19,10 +19,36 @@ namespace LayerFrontEnd.Pages {
     /// Lógica de interacción para RegisterBook.xaml
     /// </summary>
     public partial class RegisterBook : Page {
+        public bool fraAuthorNew = false;
+        public bool fraPublNew = false;
         public RegisterBook() {
             InitializeComponent();
-            fra_author.Navigate(new RegisterAuthor());
-            fra_publisher.Navigate(new RegisterPublisher());
+            fra_author.Navigate(new SelectAuthor());
+            fra_publisher.Navigate(new SelectPublisher());
+        }
+
+        private void bt_authorFrame_Click(object sender, RoutedEventArgs e) {
+            if(fraAuthorNew) {
+                fra_author.Navigate(new SelectAuthor());
+                bt_authorFrame.Content = "Nuevo";
+            }else {
+                fra_author.Navigate(new RegisterAuthor());
+                bt_authorFrame.Content = "Existente";
+            }
+
+            fraAuthorNew = !fraAuthorNew;
+        }
+
+        private void bt_publisherFrame_Click(object sender, RoutedEventArgs e) {
+            if(fraPublNew) {
+                fra_publisher.Navigate(new SelectPublisher());
+                bt_publisherFrame.Content = "Nuevo";
+            } else {
+                fra_publisher.Navigate(new RegisterPublisher());
+                bt_publisherFrame.Content = "Existente";
+            }
+
+            fraPublNew = !fraPublNew;
         }
     }
 }
