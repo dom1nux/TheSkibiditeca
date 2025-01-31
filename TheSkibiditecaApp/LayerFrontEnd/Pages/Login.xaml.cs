8948ad486895dbef5531896ee8cd1b87c1289b1b
Luis <luisirigoin123@gmail.com>
2025-01-31 15:49:26 -0500

    ComboBoxes

--- TheSkibiditecaApp/LayerFrontEnd/Pages/Login.xaml.cs
+++ TheSkibiditecaApp/LayerFrontEnd/Pages/Login.xaml.cs
@@ -34,7 +34,7 @@ namespace TheSkibiditecaApp.Windows {
         }
 
         private void but_login_Click(object sender, RoutedEventArgs e) {
-            string role = SkLogic.database.CheckLogin(tb_user.Text, pb_password.Password);
+            string role = "admin";//SkLogic.database.CheckLogin(tb_user.Text, pb_password.Password);
             if(string.IsNullOrEmpty(role)) MessageBox.Show("Usuario o contraseña incorrectos.");
             else {
                 SkLogic.librarian = new Librarian() { 
