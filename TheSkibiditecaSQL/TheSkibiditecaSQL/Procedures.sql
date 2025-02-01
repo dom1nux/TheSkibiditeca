c989f3a47af5a3ddabebfaa4ecf42bda41527a8a
Franco Diaz <go.francko.dr@gmail.com>
2025-02-01 16:26:37 -0500

    Add stored procedures to add authors, publishers and books

--- TheSkibiditecaSQL/TheSkibiditecaSQL/Procedures.sql
+++ TheSkibiditecaSQL/TheSkibiditecaSQL/Procedures.sql
@@ -177,3 +177,125 @@ BEGIN
     END CATCH
 END;
 GO
+
+-- ====================================================
+-- Descripción: Agrega libros a la tabla Libro (Book)
+-- ====================================================
+CREATE OR ALTER PROCEDURE spAddBook
+    @Title NVARCHAR(255),
+    @Cover VARBINARY(MAX) = NULL,
+    @PublicationYear INT,
+    @GenreID INT = NULL,
+    @ISBN CHAR(13),
+    @PublisherID INT = NULL,
+    @Pages INT,
+    @Language NVARCHAR(50) = NULL
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    BEGIN TRY
+        BEGIN TRANSACTION;
+        
+        INSERT INTO Book (
+            Title, 
+            Cover, 
+            PublicationYear, 
+            GenreID, 
+            ISBN, 
+            PublisherID, 
+            Pages, 
+            [Language]
+        )
+        VALUES (
+            @Title, 
+            @Cover, 
+            @PublicationYear, 
+            @GenreID, 
+            @ISBN, 
+            @PublisherID, 
+            @Pages, 
+            @Language
+        );
+        
+        DECLARE @NewBookID INT = SCOPE_IDENTITY();
+        
+        COMMIT TRANSACTION;
+        
+        SELECT @NewBookID AS NewBookID;
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+        
+        DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
+        RAISERROR(@ErrorMessage, 16, 1);
+    END CATCH
+END;
+GO
+
+-- =======================================================
+-- Descripción: Agrega autores a la tabla Autor (Author)
+-- =======================================================
+CREATE OR ALTER PROCEDURE spAddAuthor
+    @FirstName NVARCHAR(100),
+    @LastName NVARCHAR(100)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        BEGIN TRANSACTION;
+        
+        INSERT INTO Author (FirstName, LastName)
+        VALUES (@FirstName, @LastName);
+        
+        DECLARE @NewAuthorID INT = SCOPE_IDENTITY();
+        
+        COMMIT TRANSACTION;
+        
+        SELECT @NewAuthorID AS AuthorID;
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+            
+        DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
+        RAISERROR(@ErrorMessage, 16, 1);
+    END CATCH
+END;
+GO
+
+-- ==================================================================
+-- Descripción: Agrega editoriales a la tabla Editorial (Publisher)
+-- ==================================================================
+CREATE OR ALTER PROCEDURE spAddPublisher
+    @Name NVARCHAR(255),
+    @Address NVARCHAR(255) = NULL,
+    @PhoneNumber NVARCHAR(50) = NULL,
+    @Email NVARCHAR(100) = NULL
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        BEGIN TRANSACTION;
+        
+        INSERT INTO Publisher (Name, Address, PhoneNumber, Email)
+        VALUES (@Name, @Address, @PhoneNumber, @Email);
+        
+        DECLARE @NewPublisherID INT = SCOPE_IDENTITY();
+        
+        COMMIT TRANSACTION;
+        
+        SELECT @NewPublisherID AS PublisherID;
+    END TRY
+    BEGIN CATCH
+        IF @@TRANCOUNT > 0
+            ROLLBACK TRANSACTION;
+        
+        DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
+        RAISERROR(@ErrorMessage, 16, 1);
+    END CATCH
+END;
+GO
