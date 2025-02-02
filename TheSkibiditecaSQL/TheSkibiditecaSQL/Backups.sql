28238afd2da7166448b0f3ac8c8e6ab63040c3e5
Franco Diaz <go.francko.dr@gmail.com>
2025-02-01 23:11:36 -0500

    Fix comment on the backup process for the database

--- TheSkibiditecaSQL/TheSkibiditecaSQL/Backups.sql
+++ TheSkibiditecaSQL/TheSkibiditecaSQL/Backups.sql
@@ -6,7 +6,7 @@ TO DISK = '/var/opt/mssql/bkp/backup.bak';
 GO
 
 -- Se tiene que usar `docker exec -it mssql-server bash` para acceder a la shell del contenedor
--- navegamos a ` cd /var/opt/mssql`, ahi creamos un directorio para el backup con `mkdir bkp/`
+-- navegamos a `cd /var/opt/mssql`, ahi creamos un directorio para el backup con `mkdir bkp/`
 -- luego ejecutamos este query para crear el backup
 -- después copiamos el backup.bak al host con `docker cp mssql-server:/var/opt/mssql/bkp/backup.bak ~/backup.bak`
--- finalmente, copiamos el .bak al cliente a la carpeta escritorio via ssh con `scp scp -r prod:~/backup.bak C:\Users\$USER\Desktop\`
+-- finalmente, copiamos el .bak al cliente a la carpeta escritorio via ssh con `scp -r prod:~/backup.bak C:\Users\$USER\Desktop\`
